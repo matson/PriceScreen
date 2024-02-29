b21fc72dc006c679ae0e131b50a4168c2b7cdcee
Madison <madison.k.adams@gmail.com>
2024-02-29 16:59:46 -0300

    added Pod file for SVG images

---
+++ invisalertUI/Pods/Target Support Files/Pods-invisalertUI/Pods-invisalertUI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_invisalertUI : NSObject
+@end
+@implementation PodsDummy_Pods_invisalertUI
+@end
---
+++ invisalertUI/invisalertUI/PriceViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  invisalertUI
+//
+//  Created by Tracy Adams on 2/29/24.
+//
+
+import UIKit
+
+class PriceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+//        let patternImage = UIImage(named: "bg-pattern")
+//        let patternColor = UIColor(patternImage: patternImage!)
+//
+//        view.backgroundColor = patternColor
+        
+        
+    }
+    
+    //testing testing
+
+
+}
+
