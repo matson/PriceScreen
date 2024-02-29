9564e0f53950695f581ce552fec34b83437425cd
Madison <madison.k.adams@gmail.com>
2024-02-29 19:05:59 -0300

    slider and pricing functionality

--- invisalertUI/invisalertUI/PriceViewController.swift
+++ invisalertUI/invisalertUI/PriceViewController.swift
@@ -29,25 +29,25 @@ class PriceViewController: UIViewController {
     
     @IBOutlet weak var price: UILabel!
     
-    let minimumValue: Float = 10_000
-    let maximumValue: Float = 1_000_000
-    let minimumSliderValue: Float = 0
-    let maximumSliderValue: Float = 1
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let ratio = (maximumSliderValue - minimumSliderValue) / (maximumValue - minimumValue)
-            
+        let minimumValue: Float = 10000
+        let maximumValue: Float = 1000000
+        let minimumSliderValue: Float = 0
+        let maximumSliderValue: Float = 4
+        
         sliderUI.minimumValue = minimumSliderValue
         sliderUI.maximumValue = maximumSliderValue
-        sliderUI.value = (500_000 - minimumValue) * ratio + minimumSliderValue
-        
+            
         bigLabel.text = Constants.mainLabel
         midLabel.text = Constants.midLabel1
         midLabel2.text = Constants.midLabel2
+        
+       
+        //setSlider()
         
         setBackground()
         
@@ -57,16 +57,35 @@ class PriceViewController: UIViewController {
    
     //slider action
     @IBAction func slideAction(_ sender: UISlider) {
-        
-        let value = (sender.value - minimumSliderValue) / (maximumSliderValue - minimumSliderValue) * (maximumValue - minimumValue) + minimumValue
-            // Perform actions based on the slider's value
-            print("Slider value: \(value)")
-   
+        let value = Int(sender.value)
+        let formattedValue = formatValue(value)
+        pageViews.text = formattedValue + "PageViews"
+        let priceValue = calculatePrice(value)
+        price.text = String(priceValue)
+
     }
    
     
     @IBAction func pressed(_ sender: UIButton) {
         
+    }
+    
+    //to calculate price:
+    func calculatePrice(_ value: Int) -> Int {
+        switch value {
+        case 10000:
+            return 8
+        case 50000:
+            return 12
+        case 100000:
+            return 16
+        case 500000:
+            return 24
+        case 1000000:
+            return 36
+        default:
+            return 0
+        }
     }
     
     func convert(svgFileName: String, withExtension: String) -> UIImage? {
@@ -94,6 +113,25 @@ class PriceViewController: UIViewController {
         } else {
             // Handle the case where the SVG file couldn't be loaded or converted
         }
+    }
+    
+    func setSlider(){
+        //this does not work for now
+        if let sliderSVGImage = convert(svgFileName: "icon-slider", withExtension: "svg") {
+            sliderUI.setThumbImage(sliderSVGImage , for: .normal)
+        } else {
+            // Handle the case where the SVG file couldn't be loaded or converted
+        }
+    }
+    
+    func formatValue(_ value: Int) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.groupingSeparator = ","
+        formatter.groupingSize = 3
+        
+        let formattedValue = formatter.string(from: NSNumber(value: value))
+        return formattedValue ?? ""
     }
 
 
