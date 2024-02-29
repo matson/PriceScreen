3ae7c6d036cca8b9dc7f6ddd2387ee09ef30bac7
Madison <madison.k.adams@gmail.com>
2024-02-29 17:27:00 -0300

    adding components

--- invisalertUI/invisalertUI/PriceViewController.swift
+++ invisalertUI/invisalertUI/PriceViewController.swift
@@ -6,22 +6,66 @@
 //
 
 import UIKit
+import SVGKit
 
 class PriceViewController: UIViewController {
 
+    //To note: 
+    //need to convert all images from SVG -> UIimages
+    
+    @IBOutlet weak var circleImage: UIImageView!
+    
+    @IBOutlet weak var bigLabel: UILabel!
+    
+    @IBOutlet weak var midLabel: UILabel!
+    
+    @IBOutlet weak var pageViews: UILabel!
+    
+    @IBOutlet weak var sliderUI: UISlider!
+    
+    @IBOutlet weak var switchUI: UISwitch!
+    
+    @IBOutlet weak var price: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-//        let patternImage = UIImage(named: "bg-pattern")
-//        let patternColor = UIColor(patternImage: patternImage!)
-//
-//        view.backgroundColor = patternColor
+        setBackground()
         
-        
+        setCircle()
     }
     
-    //testing testing
+    @IBAction func pressed(_ sender: UIButton) {
+    }
+    
+    func convert(svgFileName: String, withExtension: String) -> UIImage? {
+        if let svgURL = Bundle.main.url(forResource: svgFileName, withExtension: withExtension) {
+            let svgData = try? Data(contentsOf: svgURL)
+            let svgImage = SVGKImage(data: svgData)
+            let svgUIImage = svgImage?.uiImage
+            return svgUIImage
+        }
+        return nil
+    }
+    
+    func setCircle(){
+        if let circleSVGImage = convert(svgFileName: "pattern-circles", withExtension: "svg") {
+            circleImage.image = circleSVGImage
+        } else {
+            // Handle the case where the SVG file couldn't be loaded or converted
+        }
+    }
+    
+    func setBackground(){
+        if let svgUIImage = convert(svgFileName: "bg-pattern", withExtension: "svg") {
+            let patternColor = UIColor(patternImage: svgUIImage)
+            view.backgroundColor = patternColor
+        } else {
+            // Handle the case where the SVG file couldn't be loaded or converted
+        }
+    }
 
 
 }
