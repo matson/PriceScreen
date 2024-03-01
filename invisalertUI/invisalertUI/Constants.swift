07d8b8137c9410817969ce2eada0cc82a2653c3f
Madison <madison.k.adams@gmail.com>
2024-02-29 23:32:39 -0300

    final commit

--- invisalertUI/invisalertUI/Constants.swift
+++ invisalertUI/invisalertUI/Constants.swift
@@ -6,12 +6,42 @@
 //
 
 import Foundation
+import UIKit
 
 struct Constants {
     
-    static let mainLabel = "Simple, traffic-based pricing"
-    static let midLabel1 = "Sign-up for our 30-day trial."
-    static let midLabel2 = "No credit card required."
+    struct Text{
+        
+        static let mainLabel = "Simple, traffic-based pricing"
+        static let midLabel1 = "Sign-up for our 30-day trial."
+        static let midLabel2 = "No credit card required."
+        static let label1 = "Unlimited websites"
+        static let label2 = "100% data ownership"
+        static let label3 = "Email reports"
+        static let monthly = "Monthly Billing"
+        static let yearly = "Yearly Billing -25%"
+        static let month = "/ month"
+        
+        
+    }
+    
+    struct Colors {
+        
+        static let lightGraytext = UIColor(hue: 225/360, saturation: 0.2, brightness: 0.6, alpha: 1.0)
+        static let darkDesatBlue = UIColor(hue: 227/360, saturation: 0.35, brightness: 0.25, alpha: 1.0)
+        static let lightGrayBlue = UIColor(hue: 223/360, saturation: 0.5, brightness: 0.87, alpha: 1.0)
+        static let emptySlideBar = UIColor(hue: 224/360, saturation: 0.65, brightness: 0.95, alpha: 1.0)
+        static let backGroundBlue = UIColor(hue: 230/360, saturation: 1.0, brightness: 0.99, alpha: 1.0)
+        static let pricingBackground = UIColor(hue: 0, saturation: 0, brightness: 1.0, alpha: 1.0)
+        
+        static let paleBlue = UIColor(hue: 226/360, saturation: 1.0, brightness: 0.87, alpha: 1.0)
+        static let discountTextRed = UIColor(hue: 15/360, saturation: 1.0, brightness: 0.7, alpha: 1.0)
+        static let discountBackground = UIColor(hue: 14/360, saturation: 0.92, brightness: 0.95, alpha: 1.0)
+        static let sliderBackground = UIColor(hue: 174/360, saturation: 0.86, brightness: 0.45, alpha: 1.0)
+        static let sliderBar = UIColor(hue: 174/360, saturation: 0.77, brightness: 0.8, alpha: 1.0)
+        
+        
+    }
     
     
     
--- invisalertUI/invisalertUI/PriceViewController.swift
+++ invisalertUI/invisalertUI/PriceViewController.swift
@@ -12,6 +12,8 @@ class PriceViewController: UIViewController {
 
     //To note: 
     //need to convert all images from SVG -> UIimages
+    //Include two stackViews to organize
+    
     
     @IBOutlet weak var circleImage: UIImageView!
     
@@ -29,6 +31,26 @@ class PriceViewController: UIViewController {
     
     @IBOutlet weak var price: UILabel!
     
+    @IBOutlet weak var check1: UIImageView!
+    
+    @IBOutlet weak var check2: UIImageView!
+    
+    @IBOutlet weak var check3: UIImageView!
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
+    
+    @IBOutlet weak var label3: UILabel!
+    
+    @IBOutlet weak var monthlyBill: UILabel!
+    
+    @IBOutlet weak var yearlyBill: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var month: UILabel!
+    
     let pageViewsValues: [Float] = [10000, 50000, 100000, 500000, 1000000]
    
     let prices: [Float] = [8, 12, 16, 24, 36]
@@ -37,47 +59,41 @@ class PriceViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-            
-        bigLabel.text = Constants.mainLabel
-        midLabel.text = Constants.midLabel1
-        midLabel2.text = Constants.midLabel2
-        
-        
+  
         sliderUI.minimumValue = 0
         sliderUI.maximumValue = Float(pageViewsValues.count - 1)
         sliderUI.value = 0
         
+        setChecks()
        
-        //setSlider()
+        setSlider()
         
-        setBackground()
+        //setBackground()
         
         setCircle()
+        
+        updateLabels()
+        
+        setAttributes()
+        
+        
     }
     
-   
     //slider action
     @IBAction func slideAction(_ sender: UISlider) {
         let roundedValue = round(sender.value)
             sender.value = roundedValue
-        
         updateLabels()
 
     }
     
-    func updateLabels() {
-        let index = Int(sliderUI.value)
-        let selectedPageViews = pageViewsValues[index]
-        let selectedPrice = prices[index]
-        
-        pageViews.text = "\(Int(selectedPageViews)) pageviews"
-        price.text = "$\(Int(selectedPrice)) /month"
+    //switch action
+    @IBAction func switchAction(_ sender: UISwitch) {
+        updateLabels()
     }
-   
     
     @IBAction func pressed(_ sender: UIButton) {
-        
+        //action with button?
     }
     
     func convert(svgFileName: String, withExtension: String) -> UIImage? {
@@ -90,6 +106,21 @@ class PriceViewController: UIViewController {
         return nil
     }
     
+    func updateLabels() {
+        let index = Int(sliderUI.value)
+        let selectedPageViews = pageViewsValues[index]
+        var selectedPrice = prices[index]
+        
+        DispatchQueue.main.async {
+              if self.switchUI.isOn {
+                  selectedPrice *= 0.75 // Apply 25% discount
+              }
+              
+              self.pageViews.text = "\(Int(selectedPageViews)) PAGEVIEWS"
+              self.price.text = "$\(Int(selectedPrice))"
+        }
+    }
+    
     func setCircle(){
         if let circleSVGImage = convert(svgFileName: "pattern-circles", withExtension: "svg") {
             circleImage.image = circleSVGImage
@@ -99,21 +130,15 @@ class PriceViewController: UIViewController {
     }
     
     func setBackground(){
-        if let svgUIImage = convert(svgFileName: "bg-pattern", withExtension: "svg") {
-            let patternColor = UIColor(patternImage: svgUIImage)
-            view.backgroundColor = patternColor
-        } else {
-            // Handle the case where the SVG file couldn't be loaded or converted
-        }
-    }
-    
-    func setSlider(){
-        //this does not work for now
-        if let sliderSVGImage = convert(svgFileName: "icon-slider", withExtension: "svg") {
-            sliderUI.setThumbImage(sliderSVGImage , for: .normal)
-        } else {
-            // Handle the case where the SVG file couldn't be loaded or converted
-        }
+        
+        //problem with having the pattern black
+//        if let svgUIImage = convert(svgFileName: "bg-pattern", withExtension: "svg") {
+//            let patternColor = UIColor(patternImage: svgUIImage)
+//            //view.backgroundColor = patternColor
+//        } else {
+//            // Handle the case where the SVG file couldn't be loaded or converted
+//        }
+        view.backgroundColor = Constants.Colors.backGroundBlue
     }
     
     func formatValue(_ value: Int) -> String {
@@ -125,6 +150,98 @@ class PriceViewController: UIViewController {
         let formattedValue = formatter.string(from: NSNumber(value: value))
         return formattedValue ?? ""
     }
+    
+    func setChecks(){
+        if let checkUIImage = convert(svgFileName: "icon-check", withExtension: "svg") {
+            check1.image = checkUIImage
+            check2.image = checkUIImage
+            check3.image = checkUIImage
+        } else {
+            // Handle the case where the SVG file couldn't be loaded or converted
+        }
+    }
+    
+    func setSlider(){
+        // Soft Cyan (Full Slider Bar): hsl(174, 77%, 80%)
+        let softCyanColor = UIColor(hue: 174/360, saturation: 0.77, brightness: 0.8, alpha: 1.0)
+        sliderUI.minimumTrackTintColor = softCyanColor
+
+        // Strong Cyan (Slider Background): hsl(174, 86%, 45%)
+        let strongCyanColor = UIColor(hue: 174/360, saturation: 0.86, brightness: 0.45, alpha: 1.0)
+        sliderUI.maximumTrackTintColor = strongCyanColor
+        
+        //problem with the image parsing?
+//        if let sliderSVGImage = convert(svgFileName: "icon-slider", withExtension: "svg") {
+//            sliderUI.setThumbImage(sliderSVGImage , for: .normal)
+//        } else {
+//            // Handle the case where the SVG file couldn't be loaded or converted
+//        }
+        
+    }
+    
+    func setAttributes(){
+        
+        pageViews.font = UIFont(name: "Manrope-ExtraBold", size: 16)
+        pageViews.textColor = Constants.Colors.lightGraytext
+        price.font = UIFont(name: "Manrope-ExtraBold", size: 46)
+        price.textColor = Constants.Colors.darkDesatBlue
+        
+        month.attributedText = NSAttributedString(string: Constants.Text.month, attributes: [
+            .font: UIFont(name: "Manrope-Bold", size: 12),
+            .foregroundColor: Constants.Colors.lightGraytext
+        ])
+        
+        button.titleLabel?.font = UIFont(name: "Manrope-Bold", size: 12)
+        button.setTitleColor(Constants.Colors.lightGrayBlue, for: .normal)
+        button.backgroundColor = Constants.Colors.darkDesatBlue
+        button.layer.cornerRadius = 16
+        button.clipsToBounds = true
+        
+        
+        monthlyBill.attributedText = NSAttributedString(string: Constants.Text.monthly, attributes: [
+            .font: UIFont(name: "Manrope-Bold", size: 12),
+            .foregroundColor: Constants.Colors.lightGraytext
+        ])
+        
+        yearlyBill.attributedText = NSAttributedString(string: Constants.Text.yearly, attributes: [
+            .font: UIFont(name: "Manrope-Bold", size: 12),
+            .foregroundColor: Constants.Colors.lightGraytext
+        ])
+        
+        
+        bigLabel.attributedText = NSAttributedString(string: Constants.Text.mainLabel, attributes: [
+            .font: UIFont(name: "Manrope-ExtraBold", size: 16),
+            .foregroundColor: Constants.Colors.darkDesatBlue
+        ])
+        
+        midLabel.attributedText = NSAttributedString(string: Constants.Text.midLabel1, attributes: [
+            .font: UIFont(name: "Manrope-Bold", size: 14),
+            .foregroundColor: Constants.Colors.lightGraytext
+        ])
+        
+        midLabel2.attributedText = NSAttributedString(string: Constants.Text.midLabel2, attributes: [
+            .font: UIFont(name: "Manrope-Bold", size: 14),
+            .foregroundColor: Constants.Colors.lightGraytext
+        ])
+        
+        label1.attributedText = NSAttributedString(string: Constants.Text.label1, attributes: [
+            .font: UIFont(name: "Manrope-Bold", size: 14),
+            .foregroundColor: Constants.Colors.lightGraytext
+        ])
+        
+        label2.attributedText = NSAttributedString(string: Constants.Text.label2, attributes: [
+            .font: UIFont(name: "Manrope-Bold", size: 14),
+            .foregroundColor: Constants.Colors.lightGraytext
+        ])
+        
+        label3.attributedText = NSAttributedString(string: Constants.Text.label3, attributes: [
+            .font: UIFont(name: "Manrope-Bold", size: 14),
+            .foregroundColor: Constants.Colors.lightGraytext
+        ])
+        
+    }
+    
+    
 
 
 }
